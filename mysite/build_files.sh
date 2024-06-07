7d4d6107191745ebdfba6e5fb28241ca6295d2e0
Rafal <r.bogusz2606@gmail.com>
2024-06-07 13:53:30 +0200

    some changes

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt 
-Python 3.10.11 manage.py collectstatic
+Python 3.10.11 manage.py collectstatic --noinput
--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'api.wsgi.app'
 
 
 # Database
@@ -130,11 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
